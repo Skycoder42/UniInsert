45f3df932852cd931e6a6ff0501c3e8f00dcb2f7
Skycoder42 <Skycoder42@users.noreply.github.com>
2016-02-28 11:49:28 +0100

    cleand up advanced search dialog

--- UnicodeUtil/advancedsearchdialog.cpp
+++ UnicodeUtil/advancedsearchdialog.cpp
@@ -10,27 +10,16 @@
 QModelIndex AdvancedSearchDialog::searchBlock(QWidget *parent, QAbstractItemModel *model)
 {
 	AdvancedSearchDialog dialog(model, parent);
-	dialog.ui->findAliasCheckBox->setVisible(false);
 	if(dialog.exec() == QDialog::Accepted)
 		return dialog.selectedIndex;
 	else
 		return QModelIndex();
 }
 
-uint AdvancedSearchDialog::searchSymbol(QWidget *parent)
-{
-	AdvancedSearchDialog dialog(parent);
-	if(dialog.exec() == QDialog::Accepted)
-		return dialog.selectedIndex.data().toUInt();
-	else
-		return UINT_MAX;
-}
-
 AdvancedSearchDialog::AdvancedSearchDialog(QAbstractItemModel *model, QWidget *parent) :
 	QDialog(parent, Qt::WindowCloseButtonHint),
 	ui(new Ui::AdvancedSearchDialog),
 	proxyModel(new QSortFilterProxyModel(this)),
-	symbolModel(Q_NULLPTR),
 	mode(DatabaseLoader::Contains)
 {
 	ui->setupUi(this);
@@ -57,112 +46,36 @@ AdvancedSearchDialog::AdvancedSearchDialog(QAbstractItemModel *model, QWidget *p
 	}
 }
 
-AdvancedSearchDialog::AdvancedSearchDialog(QWidget *parent) :
-	QDialog(parent, Qt::WindowCloseButtonHint),
-	ui(new Ui::AdvancedSearchDialog),
-	proxyModel(new QSortFilterProxyModel(this)),
-	symbolModel(Unicoder::databaseLoader()->createSearchModel(this, true)),
-	mode(DatabaseLoader::Contains)
-{
-	ui->setupUi(this);
-	SettingsDialog::loadSize(this);
-	DialogMaster::masterDialog(this);
-	this->setWindowTitle(tr("Advanced Symbol Search"));
-
-	this->ui->findAliasCheckBox->setChecked(SettingsDialog::loadValue(this, QStringLiteral("findAlias"), true).toBool());
-
-	this->proxyModel->setSourceModel(this->symbolModel);
-	this->proxyModel->setSortLocaleAware(true);
-	this->proxyModel->setSortCaseSensitivity(Qt::CaseInsensitive);
-	this->ui->treeView->setModel(this->proxyModel);
-
-	this->ui->treeView->setItemDelegateForColumn(0, new UnicodeDelegate(true, this->ui->treeView));
-	this->ui->treeView->setItemDelegateForColumn(1, new UnicodeDelegate(false, this->ui->treeView));
-
-	if(!this->ui->treeView->header()->restoreState(SettingsDialog::loadValue(this, QStringLiteral("symbolHeaderState")).toByteArray())) {
-		this->ui->treeView->resizeColumnToContents(0);
-		this->ui->treeView->sortByColumn(-1, Qt::AscendingOrder);
-	}
-}
-
 AdvancedSearchDialog::~AdvancedSearchDialog()
 {
 	SettingsDialog::storeSize(this);
-	if(this->symbolModel) {
-		SettingsDialog::storeValue(this,
-								   QStringLiteral("symbolHeaderState"),
-								   this->ui->treeView->header()->saveState());
-		SettingsDialog::storeValue(this,
-								   QStringLiteral("findAlias"),
-								   this->ui->findAliasCheckBox->isChecked());
-	} else {
-		SettingsDialog::storeValue(this,
-								   QStringLiteral("blockHeaderState"),
-								   this->ui->treeView->header()->saveState());
-	}
-
-
+	SettingsDialog::storeValue(this,
+							   QStringLiteral("blockHeaderState"),
+							   this->ui->treeView->header()->saveState());
 	delete ui;
 }
 
 void AdvancedSearchDialog::on_nameFilterLineEdit_textChanged(const QString &text)
 {
-	this->updateSearch(text,
-					   false,
-					   this->ui->findAliasCheckBox->isChecked());
-}
-
-void AdvancedSearchDialog::on_nameFilterLineEdit_returnPressed()
-{
-	this->updateSearch(this->ui->nameFilterLineEdit->text(),
-					   true,
-					   this->ui->findAliasCheckBox->isChecked());
+	QString pattern = QRegExp::escape(text);
+	pattern.replace(QStringLiteral("\\*"), QStringLiteral(".*"));
+	pattern.replace(QStringLiteral("\\?"), QStringLiteral("."));
+	if(this->mode.testFlag(DatabaseLoader::StartsWith))
+		pattern.prepend(QLatin1Char('^'));
+	if(this->mode.testFlag(DatabaseLoader::EndsWith))
+		pattern.append(QLatin1Char('$'));
+	this->proxyModel->setFilterRegExp(pattern);
 }
 
 void AdvancedSearchDialog::on_filterModeComboBox_currentIndexChanged(int index)
 {
 	this->mode = (DatabaseLoader::SearchFlag)index;
-	this->updateSearch(this->ui->nameFilterLineEdit->text(),
-					   false,
-					   this->ui->findAliasCheckBox->isChecked());
+	this->on_nameFilterLineEdit_textChanged(this->ui->nameFilterLineEdit->text());
 }
 
 void AdvancedSearchDialog::on_treeView_activated(const QModelIndex &index)
 {
 	this->selectedIndex = this->proxyModel->mapToSource(index);
-	if(this->symbolModel)
-		this->selectedIndex = this->selectedIndex.sibling(this->selectedIndex.row(), 1);
-	else
-		this->selectedIndex = this->selectedIndex.sibling(this->selectedIndex.row(), 3);
+	this->selectedIndex = this->selectedIndex.sibling(this->selectedIndex.row(), 3);
 	this->accept();
-}
-
-void AdvancedSearchDialog::on_findAliasCheckBox_toggled(bool checked)
-{
-	this->updateSearch(this->ui->nameFilterLineEdit->text(),
-					   false,
-					   checked);
-}
-
-void AdvancedSearchDialog::updateSearch(const QString &text, bool force, bool aliases)
-{
-	if(this->symbolModel) {
-		if(!force && text.size() < 3)
-			Unicoder::databaseLoader()->clearSearchModel(this->symbolModel, aliases);
-		else {
-			QString pattern = text;
-			pattern.replace(QLatin1Char('*'), QLatin1Char('%'));
-			pattern.replace(QLatin1Char('?'), QLatin1Char('_'));
-			Unicoder::databaseLoader()->searchName(pattern, this->mode, aliases, this->symbolModel);
-		}
-	} else {
-		QString pattern = QRegExp::escape(text);
-		pattern.replace(QStringLiteral("\\*"), QStringLiteral(".*"));
-		pattern.replace(QStringLiteral("\\?"), QStringLiteral("."));
-		if(this->mode.testFlag(DatabaseLoader::StartsWith))
-			pattern.prepend(QLatin1Char('^'));
-		if(this->mode.testFlag(DatabaseLoader::EndsWith))
-			pattern.append(QLatin1Char('$'));
-		this->proxyModel->setFilterRegExp(pattern);
-	}
 }
--- UnicodeUtil/advancedsearchdialog.h
+++ UnicodeUtil/advancedsearchdialog.h
@@ -3,7 +3,6 @@
 
 #include <QDialog>
 #include <QSortFilterProxyModel>
-#include <QSqlQueryModel>
 #include "databaseloader.h"
 
 namespace Ui {
@@ -16,28 +15,21 @@ class AdvancedSearchDialog : public QDialog
 
 public:
 	static QModelIndex searchBlock(QWidget *parent, QAbstractItemModel *model);
-	static uint searchSymbol(QWidget *parent);
 
 private slots:
 	void on_nameFilterLineEdit_textChanged(const QString &text);
-	void on_nameFilterLineEdit_returnPressed();
 	void on_filterModeComboBox_currentIndexChanged(int index);
 	void on_treeView_activated(const QModelIndex &index);
-	void on_findAliasCheckBox_toggled(bool checked);
 
 private:
 	Ui::AdvancedSearchDialog *ui;
 	QSortFilterProxyModel *proxyModel;
-	QSqlQueryModel *symbolModel;
 	DatabaseLoader::SearchFlags mode;
 
 	QModelIndex selectedIndex;
 
 	explicit AdvancedSearchDialog(QAbstractItemModel *model, QWidget *parent = Q_NULLPTR);
-	explicit AdvancedSearchDialog(QWidget *parent = Q_NULLPTR);
 	~AdvancedSearchDialog();
-
-	void updateSearch(const QString &text, bool force, bool aliases);
 };
 
 #endif // ADVANCEDSEARCHDIALOG_H
